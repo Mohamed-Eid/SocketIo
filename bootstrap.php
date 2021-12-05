e4d4d251c81e4cfb967922b99a62ed16f24c531a
Mohamed Eid <medoeid50@gmail.com>
2021-12-05 15:31:01 +0200

    wip

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,5 @@
 <?php
 
-
-$file = 'server.js';
-$newfile = 'ioserver.js';
-
-if (!copy($file, $newfile)) {
-    echo "failed to copy $file...\n";
-}
+echo "downloading node server .......\n";
+copy('https://raw.githubusercontent.com/Mohamed-Eid/SocketIo/main/server.js', 'socket-server.js');
+echo "downloaded \n";
